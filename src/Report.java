2aa1244f2b5f0351ff5c63d94bfe6d1ee02a2a4e
fuchsroey <fuchsroey@gmail.com>
2020-08-30 16:29:32 +0300

    add report class (logical point)

---
+++ src/Report.java
@@ -0,0 +1,11 @@
+public class Report {
+    private Point location;
+    private Boolean isIntersting;
+
+    public Report(Point p, Boolean isIntersting){
+        this.isIntersting = isIntersting;
+        this.location = p;
+    }
+
+
+}
